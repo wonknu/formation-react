69380452efa4ae2d979f415f9fe279628af2a23c
Fabrice Labbé <fabrice.labbe@cossette.com>
2020-06-09 13:49:05 -0400

    go back to header FC

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,24 +3,18 @@ import { Link } from 'react-router-dom';
 import LangSwitcher from '../langSwitcher';
 import { LocalizeContext } from '../localize';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div className="header">
-        <h1>
-        <LocalizeContext.Consumer>
-          {({t}) => t('FORMATION_REACT')}
-        </LocalizeContext.Consumer>
-        </h1>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/article/123456789">Article</Link>
-          <LangSwitcher />
-        </nav>
-      </div>
-    );
-  }
+const Header = (props) => {
+  const { t } = useContext(LocalizeContext);
+  return (
+    <div className="header">
+      <h1>{t('FORMATION_REACT')}</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/article/123456789">Article</Link>
+        <LangSwitcher />
+      </nav>
+    </div>
+  );
 };
-
 
 export default Header;
