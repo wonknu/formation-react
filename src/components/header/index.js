54f1804d0321c542cada840ee82d4efe2530012e
Fabrice Labbé <fabrice.labbe@cossette.com>
2020-06-09 13:48:47 -0400

    translate header class

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,18 +3,24 @@ import { Link } from 'react-router-dom';
 import LangSwitcher from '../langSwitcher';
 import { LocalizeContext } from '../localize';
 
-const Header = (props) => {
-  const { t } = useContext(LocalizeContext);
-  return (
-    <div className="header">
-      <h1>{t('FORMATION_REACT')}</h1>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/article/123456789">Article</Link>
-        <LangSwitcher />
-      </nav>
-    </div>
-  );
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>
+        <LocalizeContext.Consumer>
+          {({t}) => t('FORMATION_REACT')}
+        </LocalizeContext.Consumer>
+        </h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/article/123456789">Article</Link>
+          <LangSwitcher />
+        </nav>
+      </div>
+    );
+  }
 };
+
 
 export default Header;
